428cee1e82ed6beb7c381da6da1fde31e1a7654d
fajarramadhann <ramadhan28.fajar@gmail.com>
2023-12-16 20:26:44 +0700

    refactoring code

--- routes/api.js
+++ routes/api.js
@@ -3,44 +3,57 @@ import db from "../database/db.js";
 
 const router = express.Router();
 
+const STATUS_OK = "OK";
+const STATUS_ERROR = "Error";
+
+function queryAsync(sql) {
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+}
+
+async function getGuruData(noKartu) {
+  const result = await queryAsync(`SELECT * FROM guru WHERE no_kartu = '${noKartu}'`);
+  return result.length > 0 ? result[0] : null;
+}
+
+async function isAlreadyAbsenToday(noKartu, tanggalAbsen) {
+  const result = await queryAsync(`SELECT * FROM rekap_absen WHERE no_kartu = '${noKartu}' AND tanggal_absen = '${tanggalAbsen}'`);
+  return result.length > 0;
+}
+
+async function insertAbsenData(noKartu, namaGuru, kelasAjar, mataPelajaran, jamMasuk, statusKehadiran, tanggalAbsen) {
+  const sql = `INSERT INTO rekap_absen (no_kartu, nama_guru, kelas_ajar, mata_pelajaran, jam_masuk, status_kehadiran, tanggal_absen) 
+               VALUES ('${noKartu}', '${namaGuru}', '${kelasAjar}', '${mataPelajaran}', NOW(), '${statusKehadiran}', '${tanggalAbsen}')`;
+  await queryAsync(sql);
+}
+
 router.get('/absen', async (req, res) => {
   const noKartu = req.query.no_kartu;
 
   try {
-    const result_check = await queryAsync(`SELECT * FROM guru WHERE no_kartu = '${noKartu}'`);
+    const guruData = await getGuruData(noKartu);
 
-    if (result_check.length > 0) {
-      const row = result_check[0];
-      const namaGuru = row.nama_guru;
-      const kelas_ajar = row.kelas_ajar;
-      const mata_pelajaran = row.mata_pelajaran;
-
-      const jam_masuk = new Date().toLocaleTimeString('en-US', { hour12: false });
-      let status_kehadiran = "";
-
-      // const jadwal_msk = req.query.jadwal_masuk;
-      // const jadwal_masuk = await queryAsync(`SELECT * FROM jadwal WHERE jadwal_masuk = ${jadwal_msk}`)
-
-      if (jam_masuk <= '07:00:00') {
-        status_kehadiran = "Hadir";
-      } else {
-        status_kehadiran = "Hadir terlambat";
-      }
-
+    if (guruData) {
+      const { namaGuru, kelasAjar, mataPelajaran } = guruData;
+      const jamMasuk = new Date().toLocaleTimeString('en-US', { hour12: false });
+      const statusKehadiran = jamMasuk <= '07:00:00' ? "Hadir" : "Hadir terlambat";
       const tanggalAbsen = new Date().toISOString().split('T')[0];
 
-      const result_check_duplicate = await queryAsync(`SELECT * FROM rekap_absen WHERE no_kartu = '${noKartu}' AND tanggal_absen = '${tanggalAbsen}'`);
+      const isAlreadyAbsen = await isAlreadyAbsenToday(noKartu, tanggalAbsen);
 
-      if (result_check_duplicate.length > 0) {
+      if (isAlreadyAbsen) {
         res.json({ message: "Anda sudah absen hari ini" });
       } else {
-        const result_insert = await queryAsync(`INSERT INTO rekap_absen (no_kartu, nama_guru, kelas_ajar, mata_pelajaran, jam_masuk, status_kehadiran, tanggal_absen) 
-                                  VALUES ('${noKartu}', '${namaGuru}', '${kelas_ajar}', '${mata_pelajaran}', NOW(), '${status_kehadiran}', '${tanggalAbsen}')`);
+        await insertAbsenData(noKartu, namaGuru, kelasAjar, mataPelajaran, jamMasuk, statusKehadiran, tanggalAbsen);
 
         res.json({ 
           message: "Absen berhasil", 
           namaGuru: namaGuru,
-          jam: jam_masuk
+          jam: jamMasuk
         });
       }
     } else {
@@ -52,29 +65,18 @@ router.get('/absen', async (req, res) => {
   }
 });
 
-function queryAsync(sql) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
-}
-
-
-
 router.get('/kirim-uid', async (req, res) => {
   const noKartu = req.query.nokartu;
 
   try {
     await queryAsync("DELETE FROM tmp_rfid");
 
-    const sql_check = `SELECT * FROM tmp_rfid WHERE no_kartu = '${noKartu}'`;
-    const result_check = await queryAsync(sql_check);
+    const sqlCheck = `SELECT * FROM tmp_rfid WHERE no_kartu = '${noKartu}'`;
+    const resultCheck = await queryAsync(sqlCheck);
 
-    if (result_check.length === 0) {
-      const sql_insert = `INSERT INTO tmp_rfid (no_kartu) VALUES ('${noKartu}')`;
-      await queryAsync(sql_insert);
+    if (resultCheck.length === 0) {
+      const sqlInsert = `INSERT INTO tmp_rfid (no_kartu) VALUES ('${noKartu}')`;
+      await queryAsync(sqlInsert);
       res.json({ message: "UID berhasil disimpan" });
     } else {
       res.json({ message: "UID sudah ada di database" });
@@ -84,15 +86,6 @@ router.get('/kirim-uid', async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
-
-function AsyncQuery(sql) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
-}
 
 router.get('/cek-rfid', async (req, res) => {
   if (req.method === 'GET') {
@@ -105,7 +98,7 @@ router.get('/cek-rfid', async (req, res) => {
         if (result.length > 0) {
           const row = result[0];
           res.json({
-            status: "OK",
+            status: STATUS_OK,
             message: "Success",
             nama_guru: row.nama_guru 
           });
@@ -125,15 +118,6 @@ router.get('/cek-rfid', async (req, res) => {
     res.status(405).json({ message: "Method not allowed" });
   }
 });
-
-function queryAsink(sql) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
-}
 
 router.get('/jam', (req, res) => {
   const jam = new Date().toLocaleTimeString('en-US', { hour12: false });
